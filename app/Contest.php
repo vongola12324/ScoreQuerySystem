624eb7216e8ad2708a681a88dd1ea1aede51b40f
Vongola <vongola12324@coder.tw>
2015-06-11 23:18:29 +0800

    Upgrade to 5.1

--- app/Contest.php
+++ app/Contest.php
@@ -1,9 +1,44 @@
 <?php namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Contest extends Model {
 
-	//
+    protected $table = 'contests';
+    protected $fillable = ['name', 'info', 'subject'];
+    protected $hidden = [];
+
+    public function isStarted(){
+        if (empty($this->start)){
+            return true;
+        }
+        else{
+            if(Carbon::now()->timestamp > $this->start){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    public function isEnded(){
+        if (empty($this->end)){
+            return false;
+        }
+        else{
+            if(Carbon::now()->timestamp < $this->end){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+
+    public function isInProgress(){
+        return $this->isStarted() && !$this->isEnded();
+    }
 
 }
--- database/migrations/2015_06_08_083728_create_contests_table.php
+++ database/migrations/2015_06_08_083728_create_contests_table.php
@@ -16,6 +16,8 @@ class CreateContestsTable extends Migration {
 		{
 			$table->increments('id');
             $table->string('name');
+            $table->timestamp('start');
+            $table->timestamp('end');
             $table->text('subject');
 			$table->timestamps();
 		});
